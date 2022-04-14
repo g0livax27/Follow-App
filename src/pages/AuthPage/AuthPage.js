6df7c03f1ea63bbc5c1770955e9f5ea4fc6d73e5
Arthur Bernier Jr <ma2bernier@gmail.com>
2022-04-13 22:56:13 -0400

    week 19 d02 finish

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,3 +1,9 @@
+import SignUpForm from "../../components/SignUpForm/SignUpForm"
+
+
 export default function AuthPage(props){
- return (<h1>Auth Page</h1>)
+ return (<main>
+     <h1>AuthPage</h1>
+     <SignUpForm/>
+ </main>)
 }
