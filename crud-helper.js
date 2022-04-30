4d5db562da5dac2025d6da218c37332c8be3ad1c
Gerimae Oliva <gerimae.o@gmail.com>
2022-04-30 19:20:36 -0400

    cleaning up files

--- crud-helper.js
+++ crud-helper.js
@@ -5,9 +5,6 @@ require('./config/database');
 
 // Require the Mongoose models
 const User = require('./models/User');
-// const Item = require('./models/item');
-// const Category = require('./models/category');
-// const Order = require('./models/order');
 
-let user, item, category, order;
-let users, items, categories, orders;
+let user;
+let users;
