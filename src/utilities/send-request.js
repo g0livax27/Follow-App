6cab67a9d0022922323d5b6ad3a44791e16840ff
Gerimae Oliva <gerimae.o@gmail.com>
2022-04-30 22:09:42 -0400

    fixing issue with send-request.js

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -1,6 +1,6 @@
 import { getToken } from './users-service';
 
-async function sendRequest(url, method = 'GET', payload = null) {
+export default async function sendRequest(url, method = 'GET', payload = null) {
     // Fetch accepts an options object as the 2nd argument used to include a data payload, set headers, etc. 
     const options = { method };
     if (payload) {
@@ -16,4 +16,4 @@ async function sendRequest(url, method = 'GET', payload = null) {
     // res.ok will be false if the status code set to 4xx in the controller action
     if (res.ok) return res.json();
     throw new Error('Bad Request');
-}
+};
