d9cb03733add3c5c83ed681239a786090d66c88e
Gerimae Oliva <gerimae.o@gmail.com>
2022-05-02 20:06:55 -0400

    adding new components to corresponding pages

---
+++ src/components/WishListTable/WishListTable.jsx
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
+export default function WishListTable(){
+    const { month } = useParams();
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        (async() => {
+        try{
+            const response = await fetch(`http://localhost:3001/api/expenses/${month}`);
+            const data = await response.json();
+            setItems(data);
+        }catch(err){
+            console.log(err);
+        }
+        })()
+    }, []);
+
+    return(
+        <table className="table table-striped table-dark">
+            <thead>
+                <tr>
+                    <th>Wish List Item</th>
+                    <th>Price</th>
+                    <th>Purchased?</th>
+                    <th>Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                {
+                    items.map((month, i) => {
+                        return(
+                            <tr key={i}>
+                                <td>
+                                    {month.month}
+                                </td>
+                                <td>{month.name} <Link to={`/${month._id}/edit`}>Add Note/Edit</Link></td>
+                                <td>${month.amount}</td>
+                                <td>{month.complete ? 'Paid in Full' : 'No, Still Need to Pay'}</td>
+                                <td><form action={`/wishlist/${month._id}?_method=DELETE`} method="POST">
+                                    <input type="submit" value={`Delete`}/>
+                                </form></td>
+                            </tr>
+                        )
+                    })
+                }
+            </tbody>
+        </table>
+    )
+};
--- src/pages/WishListPage/WishListPage.js
+++ src/pages/WishListPage/WishListPage.js
@@ -1,58 +1,15 @@
-import { Link, useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
 import LoggedInHeader from '../../components/LoggedInHeader/LoggedInHeader';
+import WishListTable from '../../components/WishListTable/WishListTable';
 import BackButton from '../../components/BackButton/BackButton';
 import Footer from '../../components/Footer/Footer';
 
 export default function WishListPage(){
-    const { month } = useParams();
-    const [items, setItems] = useState([]);
-    
-    useEffect(() => {
-        (async() => {
-          try{
-            const response = await fetch(`http://localhost:3001/api/expenses/${month}`);
-            const data = await response.json();
-            setItems(data);
-          }catch(err){
-            console.log(err);
-          }
-        })()
-      }, []);
     return(
         <main>
             <LoggedInHeader/>
             <BackButton/>
             <h1>Wish List</h1>
-            <table className="table table-striped table-dark">
-                <thead>
-                    <tr>
-                        <th>Wish List Item</th>
-                        <th>Price</th>
-                        <th>Purchased?</th>
-                        <th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        items.map((month, i) => {
-                            return(
-                                <tr key={i}>
-                                    <td>
-                                        {month.month}
-                                    </td>
-                                    <td>{month.name} <Link to={`/${month._id}/edit`}>Add Note/Edit</Link></td>
-                                    <td>${month.amount}</td>
-                                    <td>{month.complete ? 'Paid in Full' : 'No, Still Need to Pay'}</td>
-                                    <td><form action={`/wishlist/${month._id}?_method=DELETE`} method="POST">
-                                        <input type="submit" value={`Delete`}/>
-                                    </form></td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </table>
+            <WishListTable/>
             <Footer/>
         </main>
     )
