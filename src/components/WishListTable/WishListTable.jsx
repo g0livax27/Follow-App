716c8dc08baeab4dbaafa2e58075e4c98349774d
Gerimae Oliva <gerimae.o@gmail.com>
2022-05-02 23:33:46 -0400

    updating wishlist table

--- src/components/WishListTable/WishListTable.jsx
+++ src/components/WishListTable/WishListTable.jsx
@@ -4,6 +4,7 @@ import { Link, useParams } from 'react-router-dom';
 export default function WishListTable(){
     const { month } = useParams();
     const [items, setItems] = useState([]);
+    const [refresh, setRefresh] = useState(false);
 
     useEffect(() => {
         (async() => {
@@ -15,7 +16,7 @@ export default function WishListTable(){
             console.log(err);
         }
         })()
-    }, []);
+    }, [refresh]);
 
     return(
         <table className="table table-striped table-dark">
@@ -40,7 +41,17 @@ export default function WishListTable(){
                                 <td>${month.amount}</td>
                                 <td>{month.complete ? 'Paid in Full' : 'No, Still Need to Pay'}</td>
                                 <td>
-                                    
+                                    <button onClick={(evt) => {
+                                        try{
+                                            fetch(`http://localhost:3001/api/expenses/${month._id}`, {method: 'DELETE'});
+                                        }catch(err){
+                                            console.log(err);
+                                        }finally{
+                                            setRefresh(!refresh);
+                                        }
+                                    }}>
+                                      Delete
+                                    </button>
                                 </td>
                             </tr>
                         )
