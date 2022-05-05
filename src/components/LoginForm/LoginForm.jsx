816d2d6e36486fd3c449d1fe7b596ad123996a39
Gerimae Oliva <gerimae.o@gmail.com>
2022-05-04 23:15:01 -0400

    updating forms

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import * as userService from '../../utilities/users-service';
+import * as usersService from '../../utilities/users-service';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
@@ -8,24 +8,24 @@ export default function LoginForm ({ setUser }) {
     const [credentials, setCredentials ] = useState({
         email: '',
         password: ''
-    })
+    });
     
     const [ error, setError ] = useState('')
 
     const handleChange = (evt) => {
         setCredentials({ ...credentials, [evt.target.name]: evt.target.value });
         setError('');
-    }
+    };
 
     const handleSubmit = async (evt) => {
         evt.preventDefault()
         try{
-            const user = await userService.login(credentials);
+            const user = await usersService.login(credentials);
             setUser(user)
         }catch(error){
             setError(error.message)
         }
-    }
+    };
 
     return (
         <div>
