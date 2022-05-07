223114f85827d99c5792b65507f543880db5da12
Gerimae Oliva <gerimae.o@gmail.com>
2022-05-07 00:46:40 -0400

    updating footer with icons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,14 @@
+import { RiLinkedinBoxLine } from 'react-icons/ri';
+import { RiGithubLine } from 'react-icons/ri';
+
 export default function Footer() {
     return(
         <main className="footer">
-            <h6>Gerimae Oliva - 2022</h6>
             <div className="iconLinks">
-                <p>
-                    LinkedIn
-                </p>
-                <p>
-                    GitHub
-                </p>
+                <a href='https://www.linkedin.com/in/gerimae-oliva-x0h378/'><RiLinkedinBoxLine/></a>
+                <a href='https://github.com/g0livax27'><RiGithubLine/></a>
             </div>
+            <h6>Gerimae Oliva - 2022</h6>
         </main>
     )
 };
