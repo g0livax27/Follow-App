ff0070f1fd86f9b767696e603a493fb3c8b89e5c
Gerimae Oliva <gerimae.o@gmail.com>
2022-05-30 17:12:46 -0400

    updating README.md file

--- README.md
+++ README.md
@@ -18,14 +18,14 @@ PLAN , TRACK, and SAVE by logging your expenditures from month to month.
 
 ## Getting Started:
 
+[Live Link](https://follow-app-v1.herokuapp.com/) via Heroku
+
 [Wireframe](https://miro.com/app/board/uXjVO65UKTM=/) using Miro
 ![Wire Frame](src/assets/follow-app-wireframe-screenshot.png)
 
 [Trello-Board](https://github.com/g0livax27/Follow-App/projects/1) using GitHub Projects
 
 [ERD](https://miro.com/app/board/uXjVO2gSYNs=/?share_link_id=479985147186) using Miro
-
-[Live Link](https://follow-app-v1.herokuapp.com/) via Heroku
 
 ## Unsolved Problems:
 
--- server.js
+++ server.js
@@ -14,8 +14,8 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 // Configure both serve-favicon & static middleware to serve from the production 'build' folder
-// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Check if token and create req.user
 app.use(require('./config/checkToken'));
@@ -23,16 +23,15 @@ app.use(require('./config/checkToken'));
 // Put API routes here, before the "catch all" route
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/expenses', require('./routes/api/expenses'));
-// app.use('/wishlist', require('./routes/api/wishlist'));
 
 // The following "catch all" route (note the *) is necessary to return the index.html on all non-AJAX requests
-app.get('/*', function(req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 // Configure to use port 3001 instead of 3000 during development to avoid collision with React's dev server
 const port = process.env.PORT || 3001;
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Express app running on port ${port}`)
 });
