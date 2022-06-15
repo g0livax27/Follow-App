3b2292968f5fd372d0b957eeda15a48100af37d8
Gerimae Oliva <gerimae.o@gmail.com>
2022-06-14 21:10:15 -0400

    uncommenting lines in server.js file

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 // Configure both serve-favicon & static middleware to serve from the production 'build' folder
-// app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // Check if token and create req.user
 app.use(require('./config/checkToken'));
